d20f7bfa76468489364a54a8e9c74f355d0db877
lokith <lokithk93@gmail.com>
2025-07-23 21:30:38 +0530

    Update appwrite.ts

--- services/appwrite.ts
+++ services/appwrite.ts
@@ -9,7 +9,7 @@ const client = new Client()
 
 const database = new Databases(client);
 
-export const updateSearchCount = async (query: string, movie: Movie) => {
+export const updateSearchCount = async (query: string, movie: Movie) => { 
   try {
     const result = await database.listDocuments(DATABASE_ID, COLLECTION_ID, [
       Query.equal("searchTerm", query),
